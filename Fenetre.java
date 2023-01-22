93897415002459d02b873d07492b8ac5de3b8d8d
bilel233 <47856884+bilel233@users.noreply.github.com>
2023-01-22 11:20:50 +0100

    Update Fenetre.java

--- Fenetre.java
+++ Fenetre.java
@@ -1,22 +1,62 @@
+import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 
 public class Fenetre extends JFrame {
-    public Fenetre()
-    {
-        setTitle("Ma fenetre");  /* titre de la fenetre */
-        setSize(400,300); /* taille de la fenetre */
 
+    private JLabel emailLabel;
+    private JLabel passwordLabel;
+    private JTextField emailField;
+    private JPasswordField passwordField;
+    private JButton createButton;
+
+    public Fenetre() {
+        setTitle("Creer un compte");
+        setSize(400, 300);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JPanel panel = new JPanel(); /* instanciation de l'objet' */
-        JLabel label = new JLabel("Enter Your email:");
-        JTextField textField = new JTextField(20);
-        JButton button = new JButton("Submit");
 
-        panel.add(label); /* rajout du label dans l'objet panel */
-        panel.add(textField);
-        panel.add(button);
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridBagLayout());
+        GridBagConstraints constraints = new GridBagConstraints();
+
+        emailLabel = new JLabel("Email:");
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        constraints.insets = new Insets(10, 10, 10, 10);
+        panel.add(emailLabel, constraints);
+
+        emailField = new JTextField(20);
+        constraints.gridx = 1;
+        constraints.gridy = 0;
+        panel.add(emailField, constraints);
+
+        passwordLabel = new JLabel("Password:");
+        constraints.gridx = 0;
+        constraints.gridy = 1;
+        panel.add(passwordLabel, constraints);
+
+        passwordField = new JPasswordField(20);
+        constraints.gridx = 1;
+        constraints.gridy = 1;
+        panel.add(passwordField, constraints);
+
+        createButton = new JButton("Creer un compte");
+        constraints.gridx = 0;
+        constraints.gridy = 2;
+        constraints.gridwidth = 2;
+        constraints.anchor = GridBagConstraints.CENTER;
+        createButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String email = emailField.getText();
+                char[] password = passwordField.getPassword();
+                // envoyer l'email et le mot de passe au serveur pour la creation du compte
+            }
+        });
+        panel.add(createButton, constraints);
         add(panel);
-        setVisible(true);
 
+        setVisible(true);
     }
 }
+
+
