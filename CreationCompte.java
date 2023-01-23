ba97f9c6094dd2362f0475e09a8b8f7b985e5bec
bilel233 <47856884+bilel233@users.noreply.github.com>
2023-01-23 19:30:39 +0100

    Create CreationCompte.java

---
+++ CreationCompte.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+public class CreationCompte {
+    private static final List<String> whitelist = Arrays.asList("example1@email.com", "example2@email.com", "example3@email.com");
+
+    public static boolean isEmailAllowed(String email) {
+        // Verifier si l'email est au format exact
+        if (!email.matches("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$")) {
+            return false;
+        }
+        // Verifier si l'email est dans la whitelist
+        return whitelist.contains(email);
+    }
+
+    public static void createAccount(String email, String password) {
+        if (!isEmailAllowed(email)) {
+            System.out.println("Error: Email address not allowed");
+            return;
+        }
+        //Creation du compte
+        System.out.println("Account created successfully");
+    }
+
+}
