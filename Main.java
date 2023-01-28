3b656e0853cd3a256e7d3bd78b86b976d7ab0598
bilel233 <47856884+bilel233@users.noreply.github.com>
2023-01-28 11:58:40 +0100

    mise a jour

--- Main.java
+++ Main.java
@@ -1,11 +1,39 @@
+
 import javax.swing.*;
+
+
 
 public class Main {
     public static void main(String[] args) {
+
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 new Fenetre();
+                String login = "example";
+                char[] password = "password".toCharArray();
+
             }
         });
+        CreationCompte.createAccount("example1@email.com", "password");
+        /* Utilisation de la classe User pour creer un nouvel
+        utilisateur
+         */
+        User user = new User(1, "email@example.com", "username", "password", "admin");
+
+        /* utilisation des getters pour lire les informations de l'utilisateur
+
+         */
+        String username = user.getUsername();
+        String password = user.getPassword();
+        String email = user.getEmail();
+
+        /*Utilisation des setters pour mettre a jour les informations de l'utilisateur
+
+         */
+        user.setEmail("newmail@mail.com");
+        user.setPassword("newpassword456");
+
+
+
     }
 }
