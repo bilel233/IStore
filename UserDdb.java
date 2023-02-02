0f116e64b37a50cdac1260863ee4f45bb11c86c4
bilel233 <47856884+bilel233@users.noreply.github.com>
2023-02-02 21:07:31 +0100

    Create UserDdb.java

---
+++ UserDdb.java
@@ -0,0 +1,34 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+public class UserDdb {
+    private static final String INSERT_SQL = "INSERT INTO users (email, password) VALUES (?, ?)";
+
+    public void createUser(String email, String password) {
+        try (Connection conn = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/istore", "root", "");
+             PreparedStatement statement = conn.prepareStatement(INSERT_SQL)) {
+            statement.setString(1, email);
+            statement.setString(2, hashPassword(password));
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String hashPassword(String password) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            md.update(password.getBytes());
+            byte[] hash = md.digest();
+            return Base64.getEncoder().encodeToString(hash);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
