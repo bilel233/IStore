541cd3713cec664ba4adba2468486155b03700d4
bilel233 <47856884+bilel233@users.noreply.github.com>
2023-02-07 09:27:29 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,12 @@
 
 import javax.swing.*;
-
+import java.sql.*;
+import java.util.Scanner;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 
 public class Main {
@@ -33,7 +39,43 @@ public class Main {
         user.setEmail("newmail@mail.com");
         user.setPassword("newpassword456");
 
+        /* on cree une banniere de connexion reussie apres
+        avoir rentre l'eamil et le password
 
+         */
+        boolean loginSucceesful = true;
 
+        if (loginSucceesful){
+            JFrame frame = new JFrame ("Connexion reussie");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setSize(300,200);
+            frame.setVisible(true);
+        }
+// on implemente la la classe Login avec exception d'erreur
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Entrez votre email: ");
+        String em = sc.nextLine();
+        System.out.println("Entrez votre mot de passe: ");
+        String pass = sc.nextLine();
+
+        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:8889/istore", "root", "")) {
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM Users WHERE email = ?");
+            statement.setString(1, email);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                String storedPassword = resultSet.getString("password");
+                // On Compare le mot de passe saisi par l'utilisateur avec celui stocké dans la base de données
+                if (storedPassword.equals(pass)) {
+                    System.out.println("Connexion réussie!");
+                } else {
+                    System.out.println("Mot de passe incorrect.");
+                }
+            } else {
+                System.out.println("Aucun compte trouvé avec cet email.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
-}
+
